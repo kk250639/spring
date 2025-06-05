1b317a2151d42bad70374e0be006fa38f8a495ca
kk250639 <chesyeo@gmail.com>
2025-06-05 17:01:13 +0900

    집에서 수정

---
+++ src/main/java/com/example/spring/controller/Controller03.java
@@ -0,0 +1,117 @@
+package com.example.spring.controller;
+
+import com.example.spring.dto.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Map;
+
+@Controller
+@RequestMapping("main3") // 모든 메소드의 경로 앞 부분에 적용
+public class Controller03 {
+
+    // http://localhost:8080/main3/sub1
+
+    @RequestMapping("sub1")
+    public void sub1() {
+        System.out.println("Controller03.sub1");
+    }
+
+    @RequestMapping("sub2")
+    public void sub2() {
+        System.out.println("Controller03.sub2");
+    }
+
+    // 연습:
+    // /main3/sub3 요청 올 때 일하는 메소드 작성
+    @RequestMapping("sub3")
+    public void sub3() {
+        System.out.println("Controller03.sub3");
+    }
+
+    // /main3/sub4?param1=val1&param2=val2
+    @RequestMapping("sub4")
+    public void sub4(@RequestParam("param1") String param1,
+                     @RequestParam("param2") String param2) {
+        System.out.println("param1 = " + param1);
+        System.out.println("param2 = " + param2);
+    }
+
+    // /main3/sub5?address=seoul&age=44&email=gmail&home=dokdo&city=ny&name=donald
+    @RequestMapping("sub5")
+    public void sub5(@RequestParam Map<String, String> params) {
+        for (Map.Entry<String, String> entry : params.entrySet()) {
+            System.out.println(entry.getKey() + " = " + entry.getValue());
+        }
+    }
+
+    // request parameter 를 java beans (Data Transaction Object) 으로 받기
+    // /main3/sub6?name=donald&address=la
+    // /main3/sub6?name=trump
+    // /main3/sub6?address=ny
+    @RequestMapping("sub6")
+    public void sub6(MyBean031 params) {
+        System.out.println("params.getName() = " + params.getName());
+        System.out.println("params.getAddress() = " + params.getAddress());
+    }
+
+    // 연습:
+    // /main3/sub7?name=musk&company=tesla&home=texas
+    // /main3/sub7?name=musk&home=texas
+    // /main3/sub7?name=musk&home=texas&age=55
+    // /main3/sub7?name=musk&home=texas
+    // MyBean032 javaBean(dto)를 만들어서 request parameter 받아보기
+    @RequestMapping("sub7")
+    public void sub7(MyBean032 params) {
+        System.out.println(params);
+    }
+
+    // /main3/sub8?nickName=trump&age=55&address=ny&score=98.99
+    @RequestMapping("sub8")
+    public void sub8(MyBean033 params) {
+        System.out.println(params);
+    }
+
+    // /main3/sub9?address=seoul&salary=50.5
+    @RequestMapping("sub9")
+    public void sub9(MyBean035 params) {
+        System.out.println(params);
+    }
+
+    // 연습:
+    // /main3/sub10?age=44&email=gmail&married=true&salary=40.4&name=donald
+    // 요청 파라미터 받는 MyBean036 클래스 만들고
+    // request handler method 작성
+    @RequestMapping("sub10")
+    public void sub10(MyBean036 params) {
+        System.out.println(params);
+    }
+
+    // /main3/sub11?name=donald&address=ny&pw=1234
+    // /main3/sub11?name=donald&address=ny&password=5678
+
+    // request parameter와 같은 이름의 setter 메소드를 사용
+    @RequestMapping("sub11")
+    public void sub11(MyBean037 params) {
+        System.out.println(params);
+    }
+
+
+    // /main3/sub12?name=donald&age=66&skill=work&skill=baseball&skill=java&skill=css
+    @RequestMapping("sub12")
+    public void sub12(MyBean0310 params) {
+        System.out.println(params);
+    }
+
+    // 연습
+    // /main3/sub13?hobby=cook&hobby=walk&hobby=running&score=3.3&score=4.4&score=5.5
+    // dto(MyBean0311)와 request handler method  만들기
+    @RequestMapping("sub13")
+    public void sub13(MyBean0311 params, Double[] score) {
+        System.out.println(params);
+        System.out.println(Arrays.toString(score));
+    }
+}
---
+++ src/main/java/com/example/spring/controller/Controller04.java
@@ -0,0 +1,123 @@
+package com.example.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+
+@Controller
+@RequestMapping("main4")
+public class Controller04 {
+    @RequestMapping("sub1")
+    @ResponseBody
+    public String sub1() {
+        // 1.요청 분석/가공
+        // 2.요청 처리(business logic)
+
+        // 3.결과(html) 응답
+
+        String htmlCode = """
+                <!DOCTYPE html>
+                <html>
+                <body>
+                    <h1>서버가 응답한 html 코드 🎉 </h1>
+                    <p>이렇게 html을 작성하는 것은 너무 고통스럽다</p>
+                </body>
+                </html>
+                """;
+        return htmlCode;
+    }
+
+    @RequestMapping("sub2")
+    @ResponseBody
+    public String sub2(String keyword) {
+        String weather = switch (keyword) {
+            case "서울" -> "날씨 맑음";
+            case "울릉도" -> "바람 많이 붐";
+            default -> "알 수 없음";
+        };
+
+        String htmlCode = """
+                <!DOCTYPE html>
+                <html>
+                <body>
+                    <h1>검색한 지역의 날씨</h1>
+                    <h3>""" + weather + """
+                    </h3>
+                </body>
+                </html>
+                
+                """;
+        return htmlCode;
+    }
+
+    @RequestMapping("sub3")
+    public String sub3() {
+
+        // 3. html 코드 응답
+
+        // thymeleaf html 파일 위치
+        // src/main/resources/templates/main4/sub3.html
+
+        // view 로 forward 함
+        return "main4/sub3";
+    }
+
+    // 연습:
+    // /main4/sub4 로 요청 올 때
+    // 아래 html이 최종 응답 되도록 request handler method를 작성해보세요.
+
+    // src/main/resources/templates/main4/sub4.html
+
+    @RequestMapping("sub4")
+    public String sub4() {
+        return "main4/sub4";
+    }
+
+    @RequestMapping("sub5")
+    public String sub5(Model model) {
+        // 1.요청 분석/가공
+        // 2.요청 처리(business logic)
+
+        // 3. 결과를 Model에 담기
+        model.addAttribute("name1", "결과 값1");
+        model.addAttribute("name2", "또 다른 결과");
+
+        // 4. view로 forwarding
+        return "main4/sub5";
+    }
+
+
+    // 연습:
+    // /main4/sub6 으로 요청 오면
+    // templates/main4/sub6.html 이 응답 되도록
+    // request handler method 작성
+    @RequestMapping("sub6")
+    public String sub6(Model model) {
+
+        // model attribute : model에 넣은 값(객체)
+        model.addAttribute("name", "손흥민");
+        model.addAttribute("address", "런던");
+        model.addAttribute("email", "야후");
+        
+
+        return "main4/sub6";
+    }
+
+    // 연습 : 적절한 html 코드 작성
+    @RequestMapping("sub7")
+    public String sub7(Model model) {
+        model.addAttribute("age", 55);
+        model.addAttribute("score", 97.54);
+        model.addAttribute("school", "서울대");
+        model.addAttribute("city", "신림동");
+
+        return "main4/sub7";
+    }
+
+    @RequestMapping("sub8")
+    public String sub8(Model model) {
+        model.addAttribute("number", 1234567);
+        return "main4/sub8";
+    }
+}
