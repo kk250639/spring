e1e71fac846c798df3c829cea45420e0f3602624
kk250639 <chesyeo@gmail.com>
2025-06-30 16:26:46 +0900

    js 복습

--- src/main/java/com/example/spring/Naver/repository/MemberEntityRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.spring.Naver.repository;
-
-import com.example.spring.Naver.entity.MemberEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberEntityRepository extends JpaRepository<MemberEntity, Integer> {
-}
--- src/main/java/com/example/spring/Naver/repository/PostEntityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.spring.Naver.repository;
-
-import com.example.spring.Naver.entity.PostEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface PostEntityRepository extends JpaRepository<PostEntity, Integer> {
-}
